aee7811b276b09560d37d000bd474a5feaab8291
GoldenDEagle <ionufriev95@gmail.com>
2022-11-20 20:33:36 +0200

    1

--- Assets/PixelCrew/Model/Data/InventoryData.cs
+++ Assets/PixelCrew/Model/Data/InventoryData.cs
@@ -54,3 +54,17 @@ namespace PixelCrew.Model.Data
             return null;
         }
     }
+
+    [Serializable]
+    public class InventoryItemData
+    {
+        public string Id;
+        public int Value;
+
+        public InventoryItemData(string id)
+        {
+            Id = id;
+        }
+    }
+
+}
