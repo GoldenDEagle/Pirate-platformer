53903d588133e938d13b3fa2c4ca98d44048fdeb
GoldenDEagle <ionufriev95@gmail.com>
2022-11-30 15:25:36 +0200

    Main menu & some fixes

---
+++ Assets/PixelCrew/Model/Data/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.Model.Data
+{
+    [CreateAssetMenu(menuName = "Data/GameSettings", fileName = "GameSettings")]
+    public class GameSettings : ScriptableObject
+    {
+        [SerializeField] private float Music;
+        [SerializeField] private float Sfx;
+    }
+}
---
+++ Assets/PixelCrew/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace PixelCrew.UI.Widgets.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(CustomButton), true)]
+    public class CustomButtonEditor : ButtonEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_normal"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_pressed"));
+            serializedObject.ApplyModifiedProperties();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
